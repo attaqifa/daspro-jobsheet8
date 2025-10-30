60d30bdfead4fe31d8d136ba1838b7ecdc775a76
Fadhil <attaqifa@gmail.com>
2025-10-30 11:48:53 +0700

    update NewTriangle

--- NewTriangle07.java
+++ NewTriangle07.java
@@ -5,7 +5,7 @@ public class NewTriangle07 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Masukkan nilai n = ");
         int n = sc.nextInt();
-        int i = 0;
+        int i = 1;
         while (i <= n) {
             int j = 0;
             while (j < i) {
